2c03a4173296c1aba89ae2f4a14eddda88ccbca4
Eileen McCall <eileengmccall@gmail.com>
2024-01-02 15:09:35 -0800

    feat: created navbar component and header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Link from 'next/link'
+import MainNav from '@/components/MainNav/MainNav'
 
 export default function Home() {
   return (
     <main>
-      <h1>Eileen McCall</h1>
-      <nav>
-        <ul>
-          <li><Link href="/">Home</Link></li>
-          <li><a rel="noopener noreferrer" target="_blank" href="/Resume.pdf">Resume</a></li>
-          <li><Link href="/services">Services</Link></li>
-          <li><Link href="/projects">Projects</Link></li>
-          <li><Link href="/blog">Blog</Link></li>
-          <li><Link href="/about">About</Link></li>
-        </ul>
-    
-        <ul>
-          <li><a href="https://github.com/EileenMcCall" rel="noopener noreferrer" target="_blank">Github</a></li>
-          <li>Mastodon</li>
-          <li><a href="https://www.linkedin.com/in/eileenmccall/" rel="noopener noreferrer" target="_blank">LinkedIn</a></li>
-        </ul>
-      </nav>
+      <h2>Homepage</h2>
     </main>
   )
 }
