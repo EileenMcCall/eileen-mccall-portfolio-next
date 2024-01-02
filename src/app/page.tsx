ed2a8fd62b2cf135228cf15c5273fa9945a34930
Eileen McCall <eileengmccall@gmail.com>
2024-01-02 13:24:39 -0800

    feat: created stub pages for site, added nav links to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,27 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing, bitch: &nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <h1>Eileen McCall</h1>
+      <nav>
+        <ul>
+          <li><Link href="/">Home</Link></li>
+          <li><a rel="noopener noreferrer" target="_blank" href="/Resume.pdf">Resume</a></li>
+          <li><Link href="/services">Services</Link></li>
+          <li><Link href="/projects">Projects</Link></li>
+          <li><Link href="/blog">Blog</Link></li>
+          <li><Link href="/about">About</Link></li>
+        </ul>
+    
+        <ul>
+          <li><a href="https://github.com/EileenMcCall" rel="noopener noreferrer" target="_blank">Github</a></li>
+          <li>Mastodon</li>
+          <li><a href="https://www.linkedin.com/in/eileenmccall/" rel="noopener noreferrer" target="_blank">LinkedIn</a></li>
+        </ul>
+      </nav>
     </main>
   )
 }
---
+++ src/app/services/page.tsx
@@ -0,0 +1,7 @@
+export default function Services () {
+    return (
+        <main>
+            <h2>Services</h2>
+        </main>
+    );
+}
