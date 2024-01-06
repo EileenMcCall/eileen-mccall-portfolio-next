7482e5b119c5ae91bb773f3f3d7fc963ef1c8ff2
Eileen McCall <eileengmccall@gmail.com>
2024-01-06 13:49:33 -0800

    added icon files and replaced list of technologies with icons and image tags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,24 @@ import MainNav from '@/components/MainNav/MainNav'
 import ContactForm from '@/components/ContactForm/ContactForm'
 
 export default function Home() {
+  const imageHeight = 50;
+  var tools = [
+    { alt: "JavaScript", src: "javascript.svg" },
+    { alt: "TypeScript", src: "typescript.svg" },
+    { alt: "HTML", src: "html.svg" },
+    { alt: "CSS", src: "css.svg" },
+    { alt: "React.js", src: "react.svg" },
+    { alt: "Angular", src: "angular.svg" },
+    { alt: "Next", src: "next-dark.svg" },
+    { alt: "Remix", src: "remix-icon.svg" },
+    { alt: "Astro", src: "astro.svg" },
+    { alt: "Node.js", src: "node-icon.svg" },
+    { alt: "Prisma", src: "prisma.svg" },
+    { alt: "Jest", src: "jest.svg" },
+    { alt: "MongoDB", src: "mongo.svg" },
+    { alt: "Figma", src: "figma.svg" },
+  ]
+
   return (
     <main>
       <h2>Welcome! Bienvenue! Fàilte!</h2>
@@ -16,21 +34,12 @@ export default function Home() {
       <section>
         <h3>Tools</h3>
         <p>Here are the tools that I use for my web development projects:</p>
-        <ul>
-          <li>JavaScript</li>
-          <li>TypeScript</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>React</li>
-          <li>Angular</li>
-          <li>Next.js</li>
-          <li>Remix</li>
-          <li>Astro</li>
-          <li>Node.js</li>
-          <li>Jest</li>
-          <li>Prisma</li>
-          <li>MongoDB</li>
-          <li>Figma</li>
+        <ul className={styles.toolsList}>
+          {
+            tools.map(({alt, src}, i) => (
+              <li key={i}><Image alt={alt} height={imageHeight} width={imageHeight} src={src} /></li>
+            ))
+          }
         </ul>
       </section>
       <section>
